bc36e8771c363c7b7835894245e4f0966d94a912
Carmelo <carmelo@pullara.me>
2015-09-20 17:32:52 +0200

    Deleted script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var particles = document.createElement('div');
-particles.setAttribute('id', 'main-canvas');
-
-document.getElementById('home').appendChild(particles);
-
-particlesJS.load('main-canvas', 'public/particles.json');
-
-
