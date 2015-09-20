067ef48aa69d3fc772dabb7ebd1ef82ae52b4db3
Carmelo <carmelo@pullara.me>
2015-09-20 17:32:00 +0200

    Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,38 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    watch = require('gulp-watch'),
-    babel = require('gulp-babel'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer-core'),
-    minifyCss = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+var gulp          = require('gulp');
+var less          = require('gulp-less');
+var watch         = require('gulp-watch');
+var autoprefixer  = require('gulp-autoprefixer');
+var babel         = require('gulp-babel');
+var concat        = require('gulp-concat');
+var uglify        = require('gulp-uglify');
 
-gulp.task('less', function() {
-  return gulp.src('css/style.less')
+gulp.task('styles', () => {
+  return gulp.src('./css/.less')
     .pipe(less())
-    .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css/'))
+    .pipe(autoprefixer({
+      browsers: ['> 5%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('babel', function () {
+gulp.task('babel',  () => {
   return gulp.src('js/main.js')
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest('js/build'));
 });
 
-gulp.task('autoprefixer', function () {
-  return gulp.src('css/style.css')
-    .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
-    .pipe(gulp.dest('css/'));
-});
-
-gulp.task('concat-css', function() {
+gulp.task('concat-css', () => {
   return gulp.src(['css/bootstrap.css', 'css/fontello.css', 'css/style.css'])
     .pipe(concat('all.min.css'))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('concat-js', function() {
-  return gulp.src(['js/particle.js', 'js/react.min.js'])
-    .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js/build/'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('css/*.less', ['less']);
-  gulp.watch('js/main.js', ['babel', 'concat-js']);
-  gulp.watch('css/*.css', ['autoprefixer', 'concat-css']);
+gulp.task('watch', () => {
+  gulp.watch('css/*.less', ['styles']);
+  gulp.watch('js/main.js', ['babel']);
+  gulp.watch('css/*.css', ['concat-css']);
 });
 
 gulp.task('default', ['watch']);
